c4ebbbc295d3cb98ceb4432ea14267e879b36690
Hamel Husain <hamel.husain@gmail.com>
2023-06-20 12:24:40 -0700

    clean up tests

--- quartodoc/tests/test_validation.py
+++ quartodoc/tests/test_validation.py
@@ -19,64 +19,41 @@ EXAMPLE_SECTIONS = [
                         'contents': [{'kind': 'auto', 'name': 'Builder', 'members': []}, 
                                     'Builder.from_quarto_config', 'Builder.build', 'Builder.write_index', 'Builder.write_doc_pages', 'Builder.write_sidebar', 'Builder.create_inventory']
                     },
-                    {'title': 'Inventory links', 'desc': 'Inventory files map a functions name to its corresponding url in your docs.\nThese functions allow you to create and transform inventory files.\n', 
-                     'contents': ['create_inventory', 'convert_inventory']
-                    }, 
-                    {'title': 'Data models: structural', 
-                     'desc': 'Classes for specifying the broad structure your docs.\n', 
-                     'contents': [{'kind': 'page', 
-                                'path': 'layouts-structure', 
-                                'flatten': True, 
-                                'contents': ['layout.Layout', 'layout.Section', 'layout.Page', 'layout.ContentElement']}]
-                    }, 
-                    {'title': 'Data models: docable', 'desc': 'Classes representing python objects to be rendered.\n', 
-                     'contents': [{'kind': 'page', 
-                                'path': 'layouts-docable', 
-                                'flatten': True, 
-                                'contents': [{'name': 'layout.Doc', 'members': []}, 'layout.DocFunction', 'layout.DocAttribute', 'layout.DocModule', 'layout.DocClass', 'layout.Link', 'layout.Item', 'layout.ChoicesChildren']}]
-                    }, 
-                    {'title': 'Data models: docstring patches', 
-                     'desc': 'Most of the classes for representing python objects live\nin [](`griffe.dataclasses`) or [](`griffe.docstrings.dataclasses`).\nHowever, the `quartodoc.ast` module has a number of custom classes to fill\nin support for some important docstring sections.\n', 
-                     'contents': ['ast.DocstringSectionSeeAlso', 'ast.DocstringSectionNotes', 'ast.DocstringSectionWarnings', 'ast.ExampleCode', 'ast.ExampleText']
-                     }
                 ]
 
+@pytest.fixture
+def sections():
+    return copy.deepcopy(EXAMPLE_SECTIONS)
 
+def test_valid_yaml(sections):
+    Builder(sections=sections, package='quartodoc')
 
-def test_valid_yaml():
-    Builder(sections=EXAMPLE_SECTIONS, package='quartodoc')
-
-def test_missing_title():
-    sections = copy.deepcopy(EXAMPLE_SECTIONS)
+def test_missing_title(sections):
     del sections[0]['title']
     with pytest.raises(ValueError) as e:
         Builder(sections=sections, package='quartodoc')
     assert '- Missing field `title` for element 0 in the list for `sections`' in str(e.value)
 
-
-def test_missing_desc():
+def test_missing_desc(sections):
     sections = copy.deepcopy(EXAMPLE_SECTIONS)
     del sections[2]['desc']
     with pytest.raises(ValueError) as e:
         Builder(sections=sections, package='quartodoc')
     assert '- Missing field `desc` for element 2 in the list for `sections`' in str(e.value)
 
-def test_missing_name_contents_1():
-    sections = copy.deepcopy(EXAMPLE_SECTIONS)
+def test_missing_name_contents_1(sections):
     del sections[2]['contents'][0]['name']
     with pytest.raises(ValueError) as e:
         Builder(sections=sections, package='quartodoc')
     assert '- Missing field `name` for element 0 in the list for `contents` located in element 2 in the list for `sections`' in str(e.value)
 
-def test_missing_name_contents_2():
-    sections = copy.deepcopy(EXAMPLE_SECTIONS)
+def test_missing_name_contents_2(sections):
     del sections[1]['contents'][0]['name']
     with pytest.raises(ValueError) as e:
         Builder(sections=sections, package='quartodoc')
     assert '- Missing field `name` for element 0 in the list for `contents` located in element 1 in the list for `sections`' in str(e.value)
 
-def test_misplaced_kindpage():
-    sections = copy.deepcopy(EXAMPLE_SECTIONS)
+def test_misplaced_kindpage(sections):
     sections[0]['kind'] = 'page'
     with pytest.raises(ValueError) as e:
         Builder(sections=sections, package='quartodoc')
