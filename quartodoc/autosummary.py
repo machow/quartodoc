f68fa4a0bcfc2194021289b4534be7138a335c15
Michael Chow <mc_al_github@fastmail.com>
2023-08-31 16:26:04 -0400

    feat: add render_interlinks option to Builder

--- quartodoc/autosummary.py
+++ quartodoc/autosummary.py
@@ -391,6 +391,9 @@ class Builder:
     dynamic:
         Whether to dynamically load all python objects. By default, objects are
         loaded using static analysis.
+    render_interlinks:
+        Whether to render interlinks syntax inside documented objects. Note that the
+        interlinks filter is required to generate the links in quarto.
 
     """
 
@@ -436,6 +439,7 @@ class Builder:
         source_dir: "str | None" = None,
         dynamic: bool | None = None,
         parser="numpy",
+        render_interlinks: bool = False,
     ):
         self.layout = self.load_layout(sections=sections, package=package)
 
@@ -447,6 +451,10 @@ class Builder:
         self.parser = parser
 
         self.renderer = Renderer.from_config(renderer)
+        if render_interlinks:
+            # this is a top-level option, but lives on the renderer
+            # so we just manually set it there for now.
+            self.renderer.render_interlinks = render_interlinks
 
         if out_index is not None:
             self.out_index = out_index
