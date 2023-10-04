8391268484fc0b9f90c574c87fc633c17c41a99f
mloubout <mathias.louboutin@gmail.com>
2023-10-04 10:09:34 -0400

    add example

--- quartodoc/autosummary.py
+++ quartodoc/autosummary.py
@@ -647,11 +647,8 @@ class Builder:
         yaml.dump(d_sidebar, open(self.sidebar, "w"))
 
     def _page_to_links(self, el: layout.Page):
-        if self.renderer.display_name == 'name':
-            return [{"href": f"{self.dir}/{el.path}{self.out_page_suffix}",
-                    "text": el.path.split('.')[-1]}]
-        else:
-            return [f"{self.dir}/{el.path}{self.out_page_suffix}"]
+        return [{"href": f"{self.dir}/{el.path}{self.out_page_suffix}",
+                 "text": el.contents[0].name}]
 
     # constructors ----
 
