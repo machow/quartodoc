6be1096a83d3385b91731215116a6f81765278c1
Michael Chow <mc_al_github@fastmail.com>
2023-10-13 11:53:23 -0400

    tests: clean up validation test code

--- quartodoc/tests/test_validation.py
+++ quartodoc/tests/test_validation.py
@@ -40,10 +40,6 @@ def test_missing_name_contents(snapshot):
 
     msg = check_ValueError(cfg)
     assert msg == snapshot
-    # assert (
-    #     "- Missing field `name` for element 0 in the list for `contents` located in element 1 in the list for `sections`"
-    #     in msg
-    # )
 
 
 def test_misplaced_kindpage(snapshot):
@@ -57,18 +53,5 @@ def test_misplaced_kindpage(snapshot):
 
     """
 
-    # the challenge with the pydantic feedback here is that, since Section now does
-    # not need to have title, desc, or contents. It believes the `kind: page` is a
-    # screwed up section :/.
-    # It seems like we need to intercept any error where kind is specified, and
-    # customize our message (e.g. we know kind: "page" indicates someone is trying
-    # to specify a Page)
     msg = check_ValueError(cfg)
     assert msg == snapshot
-
-    # sections[0]["kind"] = "page"
-    # msg = check_ValueError(sections)
-    # assert (
-    #     " - Missing field `path` for element 0 in the list for `sections`, which you need when setting `kind: page`."
-    #     in msg
-    # )
