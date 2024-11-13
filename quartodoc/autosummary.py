66382be73ef76a53e45b1aa586e45a51f41467af
peekxc <matt.piekenbrock@gmail.com>
2024-11-13 18:15:08 -0500

    Modifying sys.path

--- quartodoc/autosummary.py
+++ quartodoc/autosummary.py
@@ -592,8 +592,8 @@ class Builder:
 
         if self.source_dir:
             import sys
-
-            sys.path.append(self.source_dir)
+            if self.source_dir != sys.path[0]:
+                sys.path.insert(0, self.source_dir)
 
         # shaping and collection ----
 
